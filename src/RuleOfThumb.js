ad90d25636f111836b3b6a0cba889d6742d55e6a
ingmariocuberos <ing.mariocuberos@gmail.com>
2021-07-17 15:12:05 -0500

    Adding Firebase and Improving Slider

--- src/RuleOfThumb.js
+++ src/RuleOfThumb.js
@@ -1,11 +1,46 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState, Suspense } from 'react'
 import { AppRouter } from './components/routers/AppRouter'
+import { FirebaseAppProvider } from "reactfire"
+import firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/firestore";
 import { UserContext } from './components/UserContext'
 
 
 export const RuleOfThumb = () => {
 
+    const firebaseConfig = {
+    apiKey: "AIzaSyDpK4V7GxxCl4TP16a201mmiyRfdz45ct0",
+    authDomain: "react-backend-36309.firebaseapp.com",
+    databaseURL: "https://react-backend-36309-default-rtdb.firebaseio.com",
+    projectId: "react-backend-36309",
+    storageBucket: "react-backend-36309.appspot.com",
+    messagingSenderId: "740431498694",
+    appId: "1:740431498694:web:a6439361ec123aa100f6c7"
+    }
+
+    if (!firebase.apps.length) {
+        firebase.initializeApp(firebaseConfig);
+    }
+
+
+    // firebase.initializeApp(firebaseConfig);
+
     const [data, setData] = useState([]);
+
+    const [nuevaData, setNuevaData] = useState({})
+
+    const dataRef = firebase.database().ref().child('data');
+
+    dataRef.on('value', ( res ) =>{
+        setNuevaData(res.val())
+    })
+
+    useEffect(() => {
+        console.log(nuevaData)
+    }, [nuevaData])
+
+
 
     useEffect(() => {
         fetch('./assets/data.json')
@@ -21,22 +56,24 @@ export const RuleOfThumb = () => {
 
 
     return (
-        <div>
-                <UserContext.Provider 
-                    value={ 
-                        {
-                            user,
-                            setUser,
-                            data,
-                            setData
+        <FirebaseAppProvider firebaseConfig={firebaseConfig}>
+                <Suspense>
+                    <UserContext.Provider 
+                        value={ 
+                            {
+                                user,
+                                setUser,
+                                data,
+                                setData
+                            }
                         }
-                    }
-                >
-                    <AppRouter />
-                </UserContext.Provider>
+                    >
+                        <AppRouter />
+                    </UserContext.Provider>
+                </Suspense>
             
             
             
-        </div>
+        </FirebaseAppProvider>
     )
 }
--- src/components/RullingItemDesign.js
+++ src/components/RullingItemDesign.js
@@ -8,31 +8,54 @@ export const RullingItemDesign = ({
     lastUpdated,
     votes
 }) => {
+
+    const nowDate = new Date()
+    const fecha = new Date(lastUpdated);
+    const diferencia = nowDate - fecha;
+
+    
+
+    console.log(diferencia);
    
     return (
         <div 
             className="main__ruling-item-design"
             >
-                <h2 class="main_name-of-famous">{ 
-                    name.length > 21
-                    ?
-                    name.substring(0,21) + "..."
-                    :
-                    name}</h2>
+                <div className="row">
+                    <h2 className="main_name-of-famous">{ 
+                        name.length > 21
+                        ?
+                        name.substring(0,21) + "..."
+                        :
+                        name
+                        }
+                        </h2>
+                    
+                    <div className={ true ? "main__button-container main__thums-up" : "main__button-container main__thums-down"}>
+                        {
+                            true 
+                            ?
+                            <button className="icon-button" aria-label="thumbs up">
+                                    <img className="main__thumbs-svg" src="assets/img/thumbs-up.svg" alt="thumbs up" />
+                            </button>
+                            :
+                            <button className="icon-button" aria-label="thumbs down">
+                                    <img className="main__thumbs-svg" src="assets/img/thumbs-down.svg" alt="thumbs down" />
+                            </button>
+                        }
+                    </div>
+                    <div className="main__description">{ 
+                        description.length > 56
+                        ?
+                        description.substring(0,56) + "..."
+                        :
+                        description
+                        }
+                    </div>
+                </div>
+                
                 <img src={ `./assets/img/${ picture }` } className="famous-img" alt={ name }/>
 
-                {/* <div className="featured-card__buttons"> */}
-                                <button className="icon-button" aria-label="thumbs up">
-                                    <img src="assets/img/thumbs-up.svg" alt="thumbs up" />
-                                </button>
-                                <button className="icon-button" aria-label="thumbs down">
-                                    <img src="assets/img/thumbs-down.svg" alt="thumbs down" />
-                                </button>
-                            {/* </div> */}
-                
-
-
-            
         </div>
     )
 }
